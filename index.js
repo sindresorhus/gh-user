3b23b11c245720c330572c84f19acbc51defaa0c
Robin Millette <robin@millette.info>
2018-09-06 06:36:01 -0400

    Code review improvements.

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const defaultOptions = {
 	}
 };
 
-module.exports = async (username, options) => {
+module.exports = async (username, options = {}) => {
 	if (typeof username !== 'string' || !username) {
 		throw new Error('`username` required');
 	}
 
+	if (typeof options !== 'object') {
+		throw new TypeError('`options` should be an object of gh-got options');
+	}
+
+	/*
+	So...
 	if (typeof options === 'object') {
 		options = {...defaultOptions, ...options};
 	} else {
@@ -20,8 +26,10 @@ module.exports = async (username, options) => {
 		}
 		options = {...defaultOptions};
 	}
+	*/
 
-	const result = await ghGot(`users/${username}`, options);
+	// So const result = await ghGot(`users/${username}`, options);
+	const result = await ghGot(`users/${username}`, ghGot.mergeOptions(defaultOptions, options));
 	delete result.body.gravatar_id;
 	return result.body;
 };
