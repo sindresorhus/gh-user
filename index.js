2daaec296a576630f8d14e09df22cdd887062750
Robin Millette <robin@millette.info>
2018-09-06 08:26:54 -0400

    Missing commit

--- index.js
+++ index.js
@@ -11,25 +11,13 @@ module.exports = async (username, options = {}) => {
 	if (typeof username !== 'string' || !username) {
 		throw new Error('`username` required');
 	}
-
 	if (typeof options !== 'object') {
 		throw new TypeError('`options` should be an object of gh-got options');
 	}
-
-	/*
-	So...
-	if (typeof options === 'object') {
-		options = {...defaultOptions, ...options};
-	} else {
-		if (options !== undefined) {
-			throw new TypeError('`options` should be an object of gh-got options');
-		}
-		options = {...defaultOptions};
-	}
-	*/
-
-	// So const result = await ghGot(`users/${username}`, options);
-	const result = await ghGot(`users/${username}`, ghGot.mergeOptions(defaultOptions, options));
+	const result = await ghGot(
+		`users/${username}`,
+		ghGot.mergeOptions(defaultOptions, options)
+	);
 	delete result.body.gravatar_id;
 	return result.body;
 };
