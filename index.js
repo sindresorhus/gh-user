43090988005b27dd6e01753561cb1a7fe3b9c22b
Robin Millette <robin@millette.info>
2018-10-16 07:24:06 -0400

    Support all gh-got options instead of only the token (#10)

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict';
 const ghGot = require('gh-got');
 
-module.exports = async (username, token) => {
+const defaultOptions = {
+	headers: {
+		'user-agent': 'https://github.com/sindresorhus/gh-user'
+	}
+};
+
+module.exports = async (username, options = {}) => {
 	if (typeof username !== 'string' || !username) {
 		throw new Error('`username` required');
 	}
-
-	const result = await ghGot(`users/${username}`, {
-		token,
-		headers: {
-			'user-agent': 'https://github.com/sindresorhus/gh-user'
-		}
-	});
+	if (typeof options !== 'object') {
+		throw new TypeError('`options` should be an object of gh-got options');
+	}
+	const result = await ghGot(
+		`users/${username}`,
+		ghGot.mergeOptions(defaultOptions, options)
+	);
 	delete result.body.gravatar_id;
 	return result.body;
 };
